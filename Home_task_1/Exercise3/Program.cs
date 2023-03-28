a470c07697026aba1bbc79af46508bad9e905e83
VadimSD11 <71723814+VadimSD11@users.noreply.github.com>
2023-03-28 12:32:34 +0300

    Add files via upload

---
+++ Home_task_1/Exercise3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            Console.WriteLine();
+            Console.WriteLine("Цiпковський_Вадим_Задача_3");
+            Console.WriteLine();
+            int[,,] array3D = new int[,,] {
+            {  {1,1,1},
+               {1,0,1},
+               {1,1,1}
+            },
+
+            {   {1,1,1},
+                {1,0,1},
+                {1,1,1}
+            },
+             {  {1,1,1},
+                {1,0,1},
+                {1,1,1}
+            }
+        };
+
+            Cube cube = new Cube(array3D);
+            cube.Execute();
+        }
+    }
+}
