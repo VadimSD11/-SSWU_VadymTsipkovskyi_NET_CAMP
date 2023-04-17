c0111f4adbead5881b86b556e3c6574e543a5a1e
VadimSD11 <71723814+VadimSD11@users.noreply.github.com>
2023-04-17 20:43:47 +0300

    Add files via upload

---
+++ Home_Task_5/Exercise__2/Exercise__2/Size.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__2
+{
+    internal class Size
+    {
+        
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float Depth { get; set; }
+
+        public Size(float width, float height, float depth)
+        {
+            Width = width;
+            Height = height;
+            Depth = depth;
+        }
+
+        public override string? ToString()
+        {
+            return "Width := "+Width+" Height := "+Height+" Depth := "+Depth;
+        }
+    }
+}
