f548b08f5e922d9727fdd7fce6b88c1c46bde114
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-16 11:00:59 +0300

    Update Program.cs

--- Home_task_7b/Home_Task_7b_console/Home_Task_7b/Program.cs
+++ Home_task_7b/Home_Task_7b_console/Home_Task_7b/Program.cs
@@ -5,7 +5,7 @@ using static Home_Task_7b.TrafficLight;
 namespace Home_Task_7b
 {
     internal class Program
-    {
+    {//застосунок на Windows Form трішки примітивний, але він є. Цікаво, Поки що Ваш проєкт не може суміщати різні подання. 
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -14,6 +14,7 @@ namespace Home_Task_7b
             //Crossroads crossroads1 = new Crossroads(0,20);
             //crossroads1.StartSimulation();
             Console.WriteLine();
+            // А масив і цикл дл чого. На наступньому завданні з багатьма смугами довше писати прийдеться)
             TrafficLight tl1= new TrafficLight(0.50f,0.2f,0.7f);
             TrafficLight tl2 = new TrafficLight(0.50f, 0.2f, 0.7f);
             TrafficLight tl3 = new TrafficLight(0.50f, 0.2f, 0.7f);
