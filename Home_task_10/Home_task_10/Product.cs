c524e4282cdf24ecf3cea1d85de1193750e6a679
VadimSD11 <71723814+VadimSD11@users.noreply.github.com>
2023-06-04 19:36:17 +0300

    Add files via upload

---
+++ Home_task_10/Home_task_10/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_10
+{
+    internal class Product : IProduct
+    {
+        public double Weight { get; set; }
+        public double Size { get; set; }
+        public uint Days { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
---
+++ Home_task_10/Home_task_10/Task2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_10
+{
+    internal  static class Task2
+    {
+        public static void Execute() {
+            var shopVisitor = new ShopVisitor();
+            List<IProduct> components = new List<IProduct>() {  
+                new Product { Weight = 2, Size = 13, Days=4 },
+                new Electronics { Weight = 4, Size = 15, ExcessChargePercentage = 15 },
+                new Clothes { Weight = 1, Size = 2 },
+                new Product { Weight = 2, Size = 13, Days=14 }
+
+                 };
+            Client.ClientCode(components, shopVisitor);
+
+
+        }
+    }
+}
